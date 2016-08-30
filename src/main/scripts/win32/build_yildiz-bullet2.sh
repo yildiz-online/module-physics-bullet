63c84d38a3df631bf32d1afa8a3686b0499078dc
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2016-08-30 07:32:39 +0200

    [NO-JIRA] WIN32 build.

---
+++ src/main/scripts/win32/build_yildiz-bullet2.sh
@@ -0,0 +1,14 @@
+cmake . -DCMAKE_BUILD_TYPE=Release -DLIBRARY_OUTPUT_PATH="../../../../target/classes/win32" -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
+
+make
+
+cp bullet/bin/libBulletCollision.dll ../../../../target/classes/win32/libBulletCollision.dll
+cp bullet/bin/libBulletDynamics.dll ../../../../target/classes/win32/libBulletDynamics.dll
+cp bullet/bin/libBulletSoftBody.dll ../../../../target/classes/win32/libBulletSoftBody.dll
+cp bullet/bin/libLinearMath.dll ../../../../target/classes/win32/libLinearMath.dll
+cp libstdc++-6.dll ../../../../target/classes/win32/libstdc++-6.dll
+cp libgcc_s_sjlj-1.dll ../../../../target/classes/win32/libgcc_s_sjlj-1.dll
+
+
+
+
