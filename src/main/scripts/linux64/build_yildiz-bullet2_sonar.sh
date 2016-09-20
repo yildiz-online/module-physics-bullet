54bcb0e15829e6e9ddbc4e5da0e9421a339f0465
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2016-09-20 19:55:40 +0200

    [YIL-71] Prepare sonar cpp.

--- src/main/scripts/linux64/build_yildiz-bullet2_sonar.sh
+++ src/main/scripts/linux64/build_yildiz-bullet2_sonar.sh
@@ -1,6 +1,6 @@
 cmake . -DCMAKE_BUILD_TYPE=Release -DLIBRARY_OUTPUT_PATH="../../../../target/classes/linux64" -G "Unix Makefiles"
 
-build-wrapper-linux-x86-64 --out-dir /opt/atlassian/bitbucketci/agent/build/target/sonar/cpp make
+build-wrapper-linux-x86-64 --out-dir ../../../../target/sonar/cpp make
 
 rm -R CMakeFiles
 rm CMakeCache.txt
