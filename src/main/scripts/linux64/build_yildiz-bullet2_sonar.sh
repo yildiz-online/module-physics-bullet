495a70f29378789270de97da6dc7f47a22bc032d
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2016-09-21 09:24:41 +0200

    [YIL-71] Add sonar cpp.

--- src/main/scripts/linux64/build_yildiz-bullet2_sonar.sh
+++ src/main/scripts/linux64/build_yildiz-bullet2_sonar.sh
@@ -1,7 +1,7 @@
 cmake . -DCMAKE_BUILD_TYPE=Release -DLIBRARY_OUTPUT_PATH="../../../../target/classes/linux64" -G "Unix Makefiles"
 
 mkdir -p ../../../../target/sonar/cpp
-build-wrapper-linux-x86-64 --out-dir ../../../../target/sonar/cpp make
+build-wrapper-linux-x86-64 --out-dir ../../../../target/sonar-cpp make
 
 rm -R CMakeFiles
 rm CMakeCache.txt
