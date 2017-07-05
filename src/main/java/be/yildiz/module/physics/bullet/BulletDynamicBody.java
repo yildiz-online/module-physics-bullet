303e67b163fa5d45d0b76d990d6f60d62251c639
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2017-07-05 21:11:54 +0200

    [NO-JIRA] Fix for invalid file

---
+++ src/main/java/be/yildiz/module/physics/bullet/BulletDynamicBody.java
@@ -0,0 +1,106 @@
+/*
+ * This file is part of the Yildiz-Engine project, licenced under the MIT License  (MIT)
+ *
+ * Copyright (c) 2017 Grégory Van den Borre
+ *
+ * More infos available: https://www.yildiz-games.be
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without
+ * limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  SOFTWARE.
+ */
+
+package be.yildiz.module.physics.bullet;
+
+import be.yildiz.common.id.EntityId;
+import be.yildiz.common.nativeresources.NativePointer;
+import be.yildiz.common.vector.Point3D;
+import be.yildiz.common.vector.Quaternion;
+import be.yildiz.module.physics.DynamicBody;
+import jni.BulletBodyNative;
+import jni.BulletDynamicBodyNative;
+
+/**
+ * Bullet dynamic body.
+ *
+ * @author Grégory Van den Borre
+ */
+final class BulletDynamicBody extends BulletBody implements DynamicBody {
+
+    /**
+     * Native pointer for the btbody.
+     */
+    private final NativePointer pointer;
+
+    /**
+     * Contains the native calls.
+     */
+    private final BulletDynamicBodyNative bodyNative = new BulletDynamicBodyNative();
+
+    private final BulletBodyNative bulletBodyNative = new BulletBodyNative();
+
+    /**
+     * Body current mass.
+     */
+    private final float mass;
+
+    /**
+     * Full constructor.
+     *
+     * @param bodyPointer  Pointer of the associated btbody object.
+     * @param worldPointer Pointer of the associated btdiscreetworld object containing the body.
+     * @param id           Body unique identifier.
+     */
+    BulletDynamicBody(final NativePointer bodyPointer, final NativePointer worldPointer, final EntityId id, final float mass) {
+        super(bodyPointer, worldPointer, id);
+        this.pointer = bodyPointer;
+        this.mass = mass;
+    }
+
+    @Override
+    public Point3D getPosition() {
+        float[] v = this.bulletBodyNative.getPosition(this.pointer.getPointerAddress());
+        return Point3D.valueOf(v[0], v[1], v[2]);
+    }
+
+    @Override
+    public void setPosition(final float x, final float y, final float z) {
+        this.bodyNative.setPosition(this.pointer.getPointerAddress(), x, y, z);
+    }
+
+    @Override
+    public Point3D getDirection() {
+        float[] v = this.bulletBodyNative.getDirection(this.pointer.getPointerAddress());
+        return Point3D.valueOf(v[0], v[1], v[2]);
+    }
+
+    @Override
+    public void setDirection(final float x, final float y, final float z) {
+        this.bodyNative.setDirection(this.pointer.getPointerAddress(), x, y, z);
+    }
+
+    @Override
+    public void setOrientation(final Quaternion q) {
+        this.bodyNative.setOrientation(this.pointer.getPointerAddress(), q.w, q.x, q.y, q.z);
+    }
+
+    @Override
+    public void setOrientation(final float x, final float y, final float z, final float w) {
+        this.bodyNative.setOrientation(this.pointer.getPointerAddress(), w, x, y, z);
+    }
+
+    public float getMass() {
+        return this.mass;
+    }
+}
---
+++ src/main/java/be/yildiz/module/physics/bullet/BulletGhostObject.java
@@ -0,0 +1,93 @@
+/*
+ * This file is part of the Yildiz-Engine project, licenced under the MIT License  (MIT)
+ *
+ * Copyright (c) 2017 Grégory Van den Borre
+ *
+ * More infos available: https://www.yildiz-games.be
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without
+ * limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  SOFTWARE.
+ */
+
+package be.yildiz.module.physics.bullet;
+
+import be.yildiz.common.id.EntityId;
+import be.yildiz.common.nativeresources.Native;
+import be.yildiz.common.nativeresources.NativePointer;
+import be.yildiz.common.vector.Point3D;
+import be.yildiz.module.physics.AbstractMovableObject;
+import be.yildiz.module.physics.GhostObject;
+import jni.BulletGhostObjectNative;
+
+/**
+ * A ghost object is meant only to be aware of objects colliding it, it is useful as trigger for example.
+ *
+ * @author Grégory Van den Borre
+ */
+final class BulletGhostObject extends AbstractMovableObject implements GhostObject, Native {
+
+    /**
+     * Pointer address to the associated native object.
+     */
+    private final NativePointer pointer;
+
+    /**
+     * Pointer address to the world containing this object.
+     */
+    private final NativePointer worldPointer;
+
+    /**
+     * Contains the native calls.
+     */
+    private final BulletGhostObjectNative ghostNative = new BulletGhostObjectNative();
+
+    private final EntityId id;
+
+    /**
+     * Create a new instance.
+     *
+     * @param id             Unique identifier.
+     * @param pointerAddress Native address.
+     * @param world          World containing this object native address.
+     * @param position       Object position when created.
+     */
+    BulletGhostObject(final EntityId id, final NativePointer pointerAddress, final NativePointer world, final Point3D position) {
+        super(position);
+        this.id = id;
+        this.pointer = pointerAddress;
+        this.worldPointer = world;
+    }
+
+    @Override
+    public NativePointer getPointer() {
+        return this.pointer;
+    }
+
+    @Override
+    public void delete() {
+        this.ghostNative.delete(this.pointer.getPointerAddress(), this.worldPointer.getPointerAddress());
+        this.pointer.delete();
+    }
+
+    @Override
+    protected void setPositionImpl(final Point3D pos) {
+        this.ghostNative.setPosition(this.pointer.getPointerAddress(), pos.x, pos.y, pos.z);
+    }
+
+    @Override
+    public EntityId getId() {
+        return id;
+    }
+}
