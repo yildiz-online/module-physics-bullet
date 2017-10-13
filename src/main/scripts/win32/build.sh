14641012839f03b9cd768faff98fc8ad1c53c536
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2017-10-13 17:12:52 +0200

    [YMPB-4] Use bullet component start of implementation.

--- src/main/scripts/win32/build.sh
+++ src/main/scripts/win32/build.sh
@@ -3,10 +3,10 @@ cmake . -DCMAKE_BUILD_TYPE=Release -DLIBRARY_OUTPUT_PATH="../../../../target/cla
 make
 r1=$?
 
-cp bullet/bin/libBulletCollision.dll ../../../../target/classes/win32/libBulletCollision.dll
-cp bullet/bin/libBulletDynamics.dll ../../../../target/classes/win32/libBulletDynamics.dll
-cp bullet/bin/libBulletSoftBody.dll ../../../../target/classes/win32/libBulletSoftBody.dll
-cp bullet/bin/libLinearMath.dll ../../../../target/classes/win32/libLinearMath.dll
+cp bullet/win32/bin/libBulletCollision.dll ../../../../target/classes/win32/libBulletCollision.dll
+cp bullet/win32/bin/libBulletDynamics.dll ../../../../target/classes/win32/libBulletDynamics.dll
+cp bullet/win32/bin/libBulletSoftBody.dll ../../../../target/classes/win32/libBulletSoftBody.dll
+cp bullet/win32/bin/libLinearMath.dll ../../../../target/classes/win32/libLinearMath.dll
 cp libstdc++-6.dll ../../../../target/classes/win32/libstdc++-6.dll
 cp libgcc_s_sjlj-1.dll ../../../../target/classes/win32/libgcc_s_sjlj-1.dll
 
@@ -14,6 +14,7 @@ rm -R CMakeFiles
 rm CMakeCache.txt
 rm cmake_install.cmake
 rm Makefile
+
 
 return $r1
 
