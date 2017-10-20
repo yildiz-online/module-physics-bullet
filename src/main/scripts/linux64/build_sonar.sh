48427db2c5014a1c1115b706947f5cb9a39247f6
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2017-10-20 12:46:13 +0200

    [YMPB-4] Use component bullet.

--- src/main/scripts/linux64/build_sonar.sh
+++ src/main/scripts/linux64/build_sonar.sh
@@ -3,9 +3,15 @@ cmake . -DCMAKE_BUILD_TYPE=Release -DLIBRARY_OUTPUT_PATH="../../../../target/cla
 build-wrapper-linux-x86-64 --out-dir ../../../../target/sonar-cpp make
 r1=$?
 
+cp bullet/linux64/lib/libBulletCollision.so ../../../../target/classes/linux64/libBulletCollision.so
+cp bullet/linux64/lib/libBulletDynamics.so ../../../../target/classes/linux64/libBulletDynamics.so
+cp bullet/linux64/lib/libBulletSoftBody.so ../../../../target/classes/linux64/libBulletSoftBody.so
+cp bullet/linux64/lib/libLinearMath.so ../../../../target/classes/linux64/libLinearMath.so
+
 rm -R CMakeFiles
 rm CMakeCache.txt
 rm cmake_install.cmake
 rm Makefile
+rm -R bullet
 
 return $r1
