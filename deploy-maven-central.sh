c5589a1cd7048655c8528eab79d293e2e015d207
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-12-29 22:34:08 +0100

    [YE-0] Add release branch.

--- deploy-maven-central.sh
+++ deploy-maven-central.sh
@@ -8,6 +8,17 @@ if [ "$BRANCH" = "develop" ]; then
 elif [ "$BRANCH" = "master" ]; then
   openssl version -a
   openssl aes-256-cbc -pass pass:$OPENSSL_PWD -in private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s settings.xml clean deploy
+elif [ "$BRANCH" = "release" ]; then
+  git checkout develop
+  mvn versions:set -DremoveSnapshot=true
+  git commit pom.xml -m "[YE-0] Release"
+  git checkout master
+  git merge -X theirs develop
+  git push origin master
+  git checkout develop
+  mvn versions:set -DnextSnapshot=true
+  git commit pom.xml -m "[YE-0] Prepare next development version."
+  git push
 else
   mvn -V -s settings.xml clean package
 fi
