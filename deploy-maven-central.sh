e221d13d4692654e02a0d96084b59e01c0331994
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-01-20 00:03:45 +0100

    [TASK] Update parent.

--- deploy-maven-central.sh
+++ deploy-maven-central.sh
@@ -3,13 +3,12 @@
 echo "Building $BRANCH branch"
 
 if [ "$BRANCH" = "develop" ]; then
-  echo "Building development version"
   openssl version -a
   openssl aes-256-cbc -pass pass:$OPENSSL_PWD -in private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s settings.xml org.jacoco:jacoco-maven-plugin:prepare-agent clean deploy sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=$SONAR_ORGANIZATION -Dsonar.login=$SONAR
 elif [ "$BRANCH" = "master" ]; then
-  echo "Building release"
+  openssl version -a
   openssl aes-256-cbc -pass pass:$OPENSSL_PWD -in private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s settings.xml clean deploy
+  mvn -V -s settings.xml deploy -Dmaven.plugin.nexus.skip
 else
-  echo "Building unhandled branch"
   mvn -V -s settings.xml clean package
 fi
